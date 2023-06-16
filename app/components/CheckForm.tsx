b33f42a6402cd16061ec6452f065d9a561926d69
zakilagi88 <zakiramadhanii88@gmail.com>
2023-06-16 11:33:52 +0700

    fixing footer sama alert check tiket

--- app/components/CheckForm.tsx
+++ app/components/CheckForm.tsx
@@ -1,13 +1,74 @@
-import React from "react";
+"use client"
+
+import React, { useState, FormEvent } from "react";
 import Image from "next/image";
+import { IoAlertOutline } from "react-icons/io5";
+import { MdOutlineClose } from "react-icons/md";
 
-type Props = {};
+type CheckFormProps = {};
 
-const CheckForm = (props: Props) => {
+type InputErrorType = "email" | "ticketNumber";
+
+const Alert: React.FC<{
+  showAlert: boolean;
+  setShowAlert: (show: boolean) => void;
+  errorType: InputErrorType | null;
+}> = ({ showAlert, setShowAlert, errorType }) => {
+  let errorMessage = "";
+
+  if (errorType === "email") {
+    errorMessage = "Alamat email tidak valid";
+  } else if (errorType === "ticketNumber") {
+    errorMessage = "Nomor tiket diperlukan";
+  }
+
   return (
-    <form>
+    <>
+      {showAlert ? (
+        <div className="text-white px-2 py-2 border-0 rounded relative mb-2 w-[30vw] bg-red-500">
+          <span className="text-lg inline-block mr-2 align-middle">
+            <IoAlertOutline />
+          </span>
+          <span className="inline-block align-middle mr-2 leading-7 text-h3">{errorMessage}</span>
+          <button
+            className="absolute bg-transparent text-xl font-h3 leading-none right-0 top-0 mt-3 mr-6 outline-none focus:outline-none"
+            onClick={() => setShowAlert(false)}
+          >
+            <MdOutlineClose />
+          </button>
+        </div>
+      ) : null}
+    </>
+  );
+};
+
+const CheckForm: React.FC<CheckFormProps> = () => {
+  const [email, setEmail] = useState("");
+  const [ticketNumber, setTicketNumber] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
+  const [errorType, setErrorType] = useState<InputErrorType | null>(null);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    // Lakukan validasi email dan nomor tiket di sini
+    if (!email) {
+      setShowAlert(true);
+      setErrorType("email");
+    } else if (!ticketNumber) {
+      setShowAlert(true);
+      setErrorType("ticketNumber");
+    } else {
+      // Lakukan tindakan lain, seperti mengirim data ke server
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
       <div className="flex flex-col md:flex-row">
         <div className="flex flex-col space-y-4 md:space-y-6 md:w-2/5 lg:w-1/4">
+          <Alert showAlert={showAlert} setShowAlert={setShowAlert} errorType={errorType} />
+
           <div className="flex flex-col">
             <label htmlFor="email" className="text-dark text-h3 font-h3 mb-1">
               Alamat Email
@@ -18,14 +79,13 @@ const CheckForm = (props: Props) => {
               id="email"
               placeholder="Email yang digunakan sebelumnya"
               className="w-full border-[1.5px] text-base p-2 rounded-md border-grey-light focus:outline-none focus:border-dark shadow-shadow-4 placeholder:text-grey-light"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
 
           <div className="flex flex-col pb-3">
-            <label
-              htmlFor="nomor-tiket"
-              className="text-dark text-h3 font-h3 mb-1"
-            >
+            <label htmlFor="nomor-tiket" className="text-dark text-h3 font-h3 mb-1">
               Nomor Tiket
             </label>
             <input
@@ -34,6 +94,8 @@ const CheckForm = (props: Props) => {
               id="nomor-tiket"
               placeholder="Nomor tiket yang didapatkan"
               className="w-full border-[1.5px] text-base p-2 rounded-md border-grey-light focus:outline-none focus:border-dark shadow-shadow-4 placeholder:text-grey-light"
+              value={ticketNumber}
+              onChange={(e) => setTicketNumber(e.target.value)}
             />
           </div>
 
@@ -47,7 +109,7 @@ const CheckForm = (props: Props) => {
             </button>
           </div>
         </div>
-        <div className="hidden md:flex justify-center md:justify-end w-full md:w-1/2 xl:w-1/3  md:pe-4 md:mx-6 ">
+        <div className="hidden md:flex justify-center md:justify-end w-full md:w-1/2 xl:w-1/3 md:pe-4 md:mx-6 ">
           <Image
             src="/assets/img/Verify.png"
             alt="Create Ticket Illustration"
@@ -58,8 +120,7 @@ const CheckForm = (props: Props) => {
       </div>
       <div className="mt-6 sm:mt-6 mb-16 md:mb-24 text-grey-mid text-secondary">
         <p>
-          Jika ini pertama kali Anda menghubungi kami atau Anda lupa nomor
-          tiket. Silakan{" "}
+          Jika ini pertama kali Anda menghubungi kami atau Anda lupa nomor tiket. Silakan{" "}
           <span className="font-semibold underline text-black-base">
             <a href="/create-ticket">buka tiket baru</a>
           </span>
