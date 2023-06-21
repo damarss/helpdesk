05c32e23271dd06d669e034fb396ea2b4554037e
zakilagi88 <zakiramadhanii88@gmail.com>
2023-06-21 13:29:21 +0700

    ntar deh lagi

--- components/CheckForm.tsx
+++ components/CheckForm.tsx
@@ -116,7 +116,7 @@ const CheckForm = () => {
               />
               <label
                 htmlFor='email'
-                className='absolute w-full mx-2 text-[11px] lg:text-sm xl:text-base text-grey-light duration-300 transform -translate-y-4 scale-50 top-4 z-0 origin-[0] peer-focus:border-dark rounded-md peer-placeholder-shown:scale-90 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-4 peer-focus:scale-[.5] peer-focus:-translate-y-4 left-0 '
+                className='absolute w-full mx-2 text-[11px] md:text-xs xl:text-base text-grey-light duration-300 transform -translate-y-4 scale-50 top-4 z-0 origin-[0] peer-focus:border-dark rounded-md peer-placeholder-shown:scale-90 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-4 peer-focus:scale-[.5] peer-focus:-translate-y-4 left-0 '
               >
                 Email yang telah dimasukkan sebelumnya
               </label>
@@ -155,7 +155,7 @@ const CheckForm = () => {
               />
               <label
                 htmlFor='nomor-tiket'
-                className='absolute w-full mx-2 text-[11px] lg:text-sm xl:text-base text-grey-light duration-300 transform -translate-y-4 scale-50 top-4 z-0 origin-[0]  peer-focus:border-dark rounded-md peer-placeholder-shown:scale-90 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-4 peer-focus:scale-[.5] peer-focus:-translate-y-4 left-0 '
+                className='absolute w-full mx-2 text-[11px] md:text-sm xl:text-base text-grey-light duration-300 transform -translate-y-4 scale-50 top-4 z-0 origin-[0]  peer-focus:border-dark rounded-md peer-placeholder-shown:scale-90 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-4 peer-focus:scale-[.5] peer-focus:-translate-y-4 left-0 '
               >
                 Masukkan Nomor Tiket
               </label>
