6d75552d920216bd7ddab7646f690b41194eef92
Damars <damarsepti627@gmail.com>
2023-06-23 23:04:16 +0700

    Tambah tombol ganti status tiket

--- app/check-ticket/[number]/page.tsx
+++ app/check-ticket/[number]/page.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import DiscussionBody from "@/components/DiscussionBody";
 import DiscussionForm from "@/components/DiscussionForm";
 import DiscussionHead from "@/components/DiscussionHead";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { AiOutlineExperiment } from "react-icons/ai";
 
 type Params = {
   number: string;
@@ -25,48 +28,70 @@ type ticket = {
   discussions: discussion[];
 };
 
-const page = ({ params }: { params: Params }) => {
-  const ticket = {
-    number: params.number,
-    isClosed: true,
-    createdAt: "2022-09-27T00:00:00.000Z",
-    closedAt: "2022-09-27T08:00:00.000Z",
-    closedBy: "Humas Polstat STIS",
-    userName: "Rizky Wijaya",
-    userEmail: "rizkywijaya@gmail.com",
-    discussions: [
-      {
-        id: 1,
-        userName: "Rizky Wijaya",
-        message:
-          "Saya sangat tertarik untuk mendaftar sebagai mahasiswa di Polstat STIS. Bisakah Anda memberikan informasi lebih lengkap mengenai tata cara pendaftarannya? Saya ingin mengetahui langkah-langkah yang harus saya ikuti.",
-        createdAt: "2021-08-01T00:00:00.000Z",
-      },
-      {
-        id: 2,
-        userName: "Humas Polstat STIS",
-        message:
-          "Anda perlu mengisi formulir pendaftaran online dan membayar biaya pendaftaran. Selanjutnya, kirimkan dokumen-dokumen pendukung yang diminta. Setelah itu, Anda akan mengikuti ujian seleksi sesuai dengan program studi yang Anda pilih. Setelah proses seleksi selesai, Polstat STIS akan mengumumkan hasil seleksi dan jika diterima, Anda perlu melakukan registrasi dan pembayaran biaya kuliah. Jika Anda membutuhkan bantuan tambahan, kami siap membantu.",
-        createdAt: "2021-08-01T00:00:00.000Z",
-      },
-      {
-        id: 3,
-        userName: "Rizky Wijaya",
-        message:
-          "Terima kasih atas informasinya. Saya akan segera mendaftar dan mengikuti proses seleksi. Jika saya membutuhkan bantuan, saya akan menghubungi Anda kembali.",
-        createdAt: "2021-08-01T00:00:00.000Z",
-      }
-    ],
+const Page = ({ params }: { params: Params }) => {
+  const [ticket, setTicket] = useState<ticket | null>(null);
+
+  const handleChangeStatus = (): void => {
+    setTicket(ticket ? { ...ticket, isClosed: !ticket.isClosed } : null);
   };
+
+  useEffect(() => {
+    const ticketData = {
+      number: params.number,
+      isClosed: true,
+      createdAt: "2022-09-27T00:00:00.000Z",
+      closedAt: "2022-09-27T08:00:00.000Z",
+      closedBy: "Humas Polstat STIS",
+      userName: "Rizky Wijaya",
+      userEmail: "rizkywijaya@gmail.com",
+      discussions: [
+        {
+          id: 1,
+          userName: "Rizky Wijaya",
+          message:
+            "Saya sangat tertarik untuk mendaftar sebagai mahasiswa di Polstat STIS. Bisakah Anda memberikan informasi lebih lengkap mengenai tata cara pendaftarannya? Saya ingin mengetahui langkah-langkah yang harus saya ikuti.",
+          createdAt: "2021-08-01T00:00:00.000Z",
+        },
+        {
+          id: 2,
+          userName: "Humas Polstat STIS",
+          message:
+            "Anda perlu mengisi formulir pendaftaran online dan membayar biaya pendaftaran. Selanjutnya, kirimkan dokumen-dokumen pendukung yang diminta. Setelah itu, Anda akan mengikuti ujian seleksi sesuai dengan program studi yang Anda pilih. Setelah proses seleksi selesai, Polstat STIS akan mengumumkan hasil seleksi dan jika diterima, Anda perlu melakukan registrasi dan pembayaran biaya kuliah. Jika Anda membutuhkan bantuan tambahan, kami siap membantu.",
+          createdAt: "2021-08-01T00:00:00.000Z",
+        },
+        {
+          id: 3,
+          userName: "Rizky Wijaya",
+          message:
+            "Terima kasih atas informasinya. Saya akan segera mendaftar dan mengikuti proses seleksi. Jika saya membutuhkan bantuan, saya akan menghubungi Anda kembali.",
+          createdAt: "2021-08-01T00:00:00.000Z",
+        },
+      ],
+    };
+    setTicket(ticketData);
+  }, [params.number]);
 
   return (
     <main className="flex flex-col justify-center mx-7 md:mx-36">
       <DiscussionHead ticket={ticket} />
       <DiscussionBody ticket={ticket} />
-      {!ticket.isClosed && <DiscussionForm />}
+      {!ticket?.isClosed && <DiscussionForm />}
+      <div className="mb-8 md:mb-12 w-full">
+        <button
+          className="flex gap-2 w-full items-center justify-center hover:bg-biru hover:text-grey-base drop-shadow-lg border-[1px] border-biru text-biru text-active font-active rounded-md p-3 md:px-5 md:py-1"
+          onClick={handleChangeStatus}
+        >
+          <span className="text-2xl">
+            <AiOutlineExperiment />
+          </span>
+          <span className="">
+            Ubah Status Tiket <sub>*hanya untuk pengujian</sub>
+          </span>
+        </button>
+      </div>
     </main>
   );
 };
 
-export default page;
+export default Page;
 export type { ticket, discussion };
--- components/DiscussionBody.tsx
+++ components/DiscussionBody.tsx
@@ -4,7 +4,7 @@ import { ticket } from "@/app/check-ticket/[number]/page";
 import { LuCheckSquare } from "react-icons/lu";
 
 type Props = {
-  ticket: ticket;
+  ticket: ticket | null;
 };
 
 const DiscussionBody = (props: Props) => {
@@ -13,8 +13,8 @@ const DiscussionBody = (props: Props) => {
   return (
     <div className="mb-4 md:mb-6">
       <span className="block text-secondary text-grey-light mb-10 text-center">
-        Dibuka oleh {ticket.userName} pada{" "}
-        {new Date(ticket.createdAt).toLocaleDateString("id", {
+        Dibuka oleh {ticket?.userName} pada{" "}
+        {ticket && new Date(ticket?.createdAt).toLocaleDateString("id", {
           day: "numeric",
           month: "long",
           year: "numeric",
@@ -23,7 +23,7 @@ const DiscussionBody = (props: Props) => {
         })}
       </span>
       {ticket &&
-        ticket.discussions.map((discussion) => (
+        ticket?.discussions.map((discussion) => (
           <DiscussionCard
             key={discussion.id}
             creator={ticket.userName}
@@ -32,9 +32,9 @@ const DiscussionBody = (props: Props) => {
             createdAt={discussion.createdAt}
           />
         ))}
-      {ticket.isClosed && (<div className="flex space-x-2 items-center">
+      {ticket?.isClosed && (<div className="flex space-x-2 items-center">
         <LuCheckSquare className="text-xl text-green" />
-        <p className="text-secondary text-grey-light">Ditutup oleh {ticket?.closedBy} dengan status selesai pada {new Date(ticket?.closedAt || "").toLocaleDateString("id", {day: "numeric", month: "long", year: "numeric", hour: "numeric", minute: "numeric"})}</p>
+        <p className="text-secondary text-grey-light">Ditutup oleh {ticket?.closedBy} dengan status selesai pada {new Date(ticket?.closedAt || "").toLocaleDateString("id", {day: "numeric", month: "long", year: "numeric", hour: "numeric", minute: "numeric"})}.</p>
       </div>)}
     </div>
   );
